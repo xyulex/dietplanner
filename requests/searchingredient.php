cef13e7b552fae1ad6400f91685e9ffcc175ff91
Raul Martinez <raulmartinez911@hotmail.com>
2014-09-24 17:15:02 +0200

    Solucionado problema acentos en búsqueda ingredientes

--- requests/searchingredient.php
+++ requests/searchingredient.php
@@ -1,8 +1,10 @@
 <?php
-
+	header('Content-Type: text/html; charset=utf-8');
     $conn = mysql_connect("localhost", "root", "")  or die(mysql_error());
+    mysql_set_charset('utf8');
     mysql_select_db("diet", $conn) or die(mysql_error());
-    $q = utf8_decode(strtolower($_GET["term"]));
+    
+    $q = strtolower($_GET["term"]);
 
   $return = array();
     $query = mysql_query("select id,name from ingredients_name where name like '%$q%'") or die(mysql_error());
